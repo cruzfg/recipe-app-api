765a4589f25b77d14add5fa62ab4e534b91cc005
F. Gabriel Cruz <cruzfg@gmail.com>
2019-07-04 11:25:13 -0600

    fixed requirements file, for some reason it was missing require package that I'd put in before. Put in db wait tests.

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(TestCase):
+        """Test waiting for db when db is available"""
+        # This is setting up the mock
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            #This is setting up what it is going to return.
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test wait for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6) 
